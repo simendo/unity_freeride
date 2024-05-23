a6dd8e29d717ac758c5fa9a2f8a834a99f3e5144
simendo <simendo@stud.ntnu.no>
2024-05-23 18:46:13 +0200

    Merge Python scripts folder from main into master

--- python_scripts/color_extract.py
+++
@@ -1,9 +0,0 @@
-from colorthief import ColorThief
-
-color_thief = ColorThief('/Users/simendomaas/Documents/Skolearbeid/Masteroppgave/Media/IMG_9560.jpg')
-# Get the dominant color
-dominant_color = color_thief.get_color(quality=1)
-print(dominant_color)
-
-palette = color_thief.get_palette(color_count=2, quality=1)
-print(palette)
